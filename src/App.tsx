649d3b045936deb7b9ad1d3e669878a084e44877
Ryan Ziegler <rzig408@gmail.com>
2021-10-28 11:52:20 -0500

    create accessible ChannelsList component with no tests

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
+import { useState } from "react";
 import "./App.css";
+import ChannelsList from "./components/ChannelsList";
 
 function App() {
+  const [selected, setSelected] = useState({
+    group: "Group 1",
+    channel: "channel-2",
+  });
+
   return (
-    <div className="App">
-      <div className="bg-blue-300">Hello this is blue</div>
-    </div>
+    <>
+      <ChannelsList
+        groups={["Group 1", "Group 2"]}
+        channelsByGroup={{
+          "Group 1": ["channel-1", "channel-2"],
+          "Group 2": ["channel-3", "channel-4"],
+        }}
+        selected={selected}
+        onSelect={(selection) => setSelected(selection)}
+        serverName="CS222 Discord"
+      />
+    </>
   );
 }
 
