68e7a2b7c2d4d1dd289a0d6e1e5c73a4a627ea90
Ryan Ziegler <rzig408@gmail.com>
2021-12-04 17:26:47 -0600

    create servers list

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import "./App.css";
 import ChannelsList from "./components/ChannelsList/";
+import ServersList from "./components/ServersList";
 import { ShortcutProvider } from "./contexts/ShortcutContext/ShortcutContext";
 
 function App() {
@@ -9,18 +10,33 @@ function App() {
     channel: "channel-2",
   });
 
+  const [selectedServer, setSelectedServer] = useState({
+    group: "FA21",
+    channel: "PHYS212",
+  });
+
   return (
     <ShortcutProvider>
-      <ChannelsList
-        groups={["Group 1", "Group 2"]}
-        channelsByGroup={{
-          "Group 1": ["channel-1", "channel-2"],
-          "Group 2": ["channel-3", "channel-4"],
-        }}
-        selected={selected}
-        onSelect={(selection) => setSelected(selection)}
-        serverName="CS222 Discord"
-      />
+      <div style={{ display: "flex", flexDirection: "row", width: "100vw" }}>
+        <ServersList
+          groups={["FA21"]}
+          serversByGroup={{
+            FA21: ["PHYS212", "CS222"],
+          }}
+          selected={selectedServer}
+          onSelect={(selection) => setSelectedServer(selection)}
+        />
+        <ChannelsList
+          groups={["Group 1", "Group 2"]}
+          channelsByGroup={{
+            "Group 1": ["channel-1", "channel-2"],
+            "Group 2": ["channel-3", "channel-4"],
+          }}
+          selected={selected}
+          onSelect={(selection) => setSelected(selection)}
+          serverName="CS222 Discord"
+        />
+      </div>
     </ShortcutProvider>
   );
 }
